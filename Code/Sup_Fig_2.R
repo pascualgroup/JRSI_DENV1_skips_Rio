5d7fcb77f8093909b3d24a9c54bc1565bf92b3ad
Rahul Subramnian <rsfive@uchicago.edu>
2020-04-13 17:59:59 -0500

    Updated Sup Fig 2 Going to re-format now.

--- Code/Sup_Fig_2.R
+++ Code/Sup_Fig_2.R
@@ -25,23 +25,21 @@ rahul_poster_theme = theme(
                              color = "black")
 )
 model_name_list = c("A_7", "A_6",
-                    "A_5", "A_3" )
-model_label_list = factor(c("SIR Cosine No Immigration", "SIR Spline No Immmigration",
-                         "SEIR Spline No Immigration", "SEIR Spline Immigration" ))
-model_label_list = factor(model_label_list, levels = c("SIR Cosine No Immigration", "SIR Spline No Immmigration",
-                            "SEIR Spline No Immigration", "SEIR Spline Immigration" ))
+                    "A_5")
+model_label_list = factor(c("SIR Cosine", "SIR Spline",
+                         "SEIR Spline"))
+model_label_list = factor(model_label_list, levels = c("SIR Cosine", "SIR Spline",
+                            "SEIR Spline", "SEIR Spline" ))
 
 Csnippet_file_path_list = c("Csnippet_SIR_cosine_model.R",
                             "Csnippet_SIR_spline_model.R",
-                            "Csnippet_SEIR_spline_model.R",
                             "Csnippet_SEIR_spline_model.R")
-Num_est_parameters_list = c(7,7,8,9)
+Num_est_parameters_list = c(7,7,8)
 data_file_path_list = c("../Generated_Data/Rio_DENV1_Data_2_25_years_clean.csv",
                         "../Generated_Data/Rio_DENV1_Data_2_25_years_clean.csv",
-                        "../Generated_Data/Rio_DENV1_Data_2_25_years_clean.csv",
-                        "../Generated_Data/Rio_DENV1_Data_3_75_years_clean.csv")
+                        "../Generated_Data/Rio_DENV1_Data_2_25_years_clean.csv")
 
-num_years_list = c(2.50, 2.50, 2.50, 4)
+num_years_list = c(2.50, 2.50, 2.50)
 
 model_ref_df = data.frame(model_name = model_name_list, model_label = model_label_list,
                           Csnippet_file_path = Csnippet_file_path_list,
@@ -345,22 +343,136 @@ rahul_panel_theme = theme(
                             color = "black")
 )
 
-Sup_Fig_2_A_comb = Sup_Fig_2_A + rahul_panel_theme + theme(legend.position = "None") 
+Sup_Fig_2_A_comb = Sup_Fig_2_A + rahul_panel_theme + theme(legend.position = c(.75, .55))
 Sup_Fig_2_B_comb = Sup_Fig_2_B + rahul_panel_theme + theme(legend.position = "None")
-Sup_Fig_2_C_comb = Sup_Fig_2_C + rahul_panel_theme + theme(legend.position = "None")
-Sup_Fig_2_D_comb = Sup_Fig_2_D + rahul_panel_theme + theme(legend.position = "None")
-Sup_Fig_2_E_comb = Sup_Fig_2_E + rahul_panel_theme + theme(legend.position = "None")
-Sup_Fig_2_A_small_legend = Sup_Fig_2_A + rahul_panel_theme
-legend <- cowplot::get_legend(Sup_Fig_2_A_small_legend)
-
-Sup_Fig_2_A_leg = grid.draw(legend)
 
 
+Sup_Figure_3_D_df_colnames = c("time", "R_0", "R_0_min", "R_0_max", "Year", "Days_in_Year", "Month", "Month_Name", "Model", "Model_Name")
+Sup_Figure_3_D_label_df_colnames = c("plot_label_times", "plot_label_month_names", "Model", "Model_Name")
+
+
+Sup_Figure_3_D_df = data.frame(matrix(nrow = 0, ncol = length(Sup_Figure_3_D_df_colnames)))
+Sup_Figure_3_D_label_df = data.frame(matrix(nrow = 0, ncol = length(Sup_Figure_3_D_label_df_colnames)))
+
+
+colnames(Sup_Figure_3_D_df) = Sup_Figure_3_D_df_colnames
+colnames(Sup_Figure_3_D_label_df) = Sup_Figure_3_D_label_df_colnames
+
+for(model_index in seq(1:length(model_name_list))){
+  print(model_index)
+  model_name = as.character(model_name_list[model_index])
+  single_model_ref_data = filter(model_ref_df, model_name == !!model_name)
+  model_label = single_model_ref_data$model_label
+  Csnippet_file_path = single_model_ref_data$Csnippet_file_path
+  Num_est_parameters = single_model_ref_data$Num_est_parameters
+  data_file_path = single_model_ref_data$data_file_path
+  num_years = single_model_ref_data$num_years
+  
+  Rio_data_clean = read.csv(file = data_file_path)
+  
+  Rio_clean_data = Rio_data_clean
+  #head(Rio_data_clean)
+  
+  source(Csnippet_file_path, local = TRUE)
+  
+  #Set t0
+  t0 = as.numeric(as.Date("1986/05/01") - as.Date("1986/01/01"))
+  
+  all_combo_data = read.csv(paste0("../Generated_Data/Profiles/", model_name, "_Model/",model_name, "_Model_BP_top_2_LL_all_params_sim_cases_data.csv"))
+  
+  all_R0_data = read.csv(paste0("../Generated_Data/Profiles/", model_name, "_Model/",model_name, "_Model_BP_top_2_LL_all_params_sim_R0_data.csv"))
+  
+  all_combo_S_data = read.csv(paste0("../Generated_Data/Profiles/", model_name, "_Model/",model_name, "_Model_BP_top_2_LL_all_params_sim_S_over_N_data.csv"))
+  
+  all_R_eff_data = read.csv(paste0("../Generated_Data/Profiles/", model_name, "_Model/",model_name, "_Model_BP_top_2_LL_all_params_sim_Reff_data.csv"))
+  
+  bio_good_2_LL = read.csv(paste0("../Generated_Data/Profiles/", model_name, "_Model/",model_name, "_Model_BP_top_2_LL_all_params_bio_good_2_LL.csv"))
+  
+
+  
+  ## Data for Supplemental Figure 2D
+  R_0_max = aggregate(R_0~time, all_R0_data, FUN = max)
+  R_0_max = dplyr::select(R_0_max, time = time, R_0_max = R_0)
+  R_0_min = aggregate(R_0~time, all_R0_data, FUN = min)
+  R_0_min = dplyr::select(R_0_min, time = time, R_0_min = R_0)
+  
+  ML_combo_num = which(bio_good_2_LL$LL == max(bio_good_2_LL$LL))
+  
+  ML_R0_df = filter(all_R0_data, combo_num == ML_combo_num)
+  ML_R0_df = dplyr::select(ML_R0_df, time = time, R_0_MLE = R_0)
+  R_0_ribbon_df = join(R_0_min, R_0_max)
+  R_0_ribbon_df = join(R_0_ribbon_df, ML_R0_df)
+  
+  R_0_ribbon_df_melt = melt(R_0_ribbon_df, id.vars = c("time", "R_0_min", "R_0_max" ))
+  ribbon_label_3_D = "R_0 range \n  (All  2 LL Combinations)"
+  
+  R_0_ribbon_df_melt$Ribbon_label = ribbon_label_3_D
+  
+  
+  R_0_plot_data = R_0_ribbon_df_melt
+  
+  R_0_plot_data$Year = R_0_plot_data$time/365
+  R_0_plot_data$Days_in_Year = (R_0_plot_data$time%%365)
+  R_0_plot_data$Month = round((R_0_plot_data$Days_in_Year/365)*12) + 1
+  single_year_R_0_data= filter(R_0_plot_data, Year <=  2 & Year >= 1 )
+  single_year_R_0_data$Month_Name = c(month.abb, month.abb[1])
+  
+  single_year_R_0_data$Model = model_name
+  single_year_R_0_data$Model_Name = model_label
+  Sup_Figure_3_D_df = rbind(Sup_Figure_3_D_df, single_year_R_0_data)
+  
+  plot_label_months =seq(from = 1, to = 13, by = 2)
+  plot_label_month_names = single_year_R_0_data$Month_Name[plot_label_months]
+  plot_label_times = single_year_R_0_data$time[plot_label_months]
+  
+  plot_label_months = seq(from = 1.00, to = 13.00, length = 7)
+  
+  single_model_Sup_Figure_3_D_label_df = data.frame(plot_label_times, plot_label_month_names)
+  single_model_Sup_Figure_3_D_label_df$Model = model_name
+  single_model_Sup_Figure_3_D_label_df$Model_Name = model_label
+  
+  Sup_Figure_3_D_label_df = rbind(Sup_Figure_3_D_label_df, single_model_Sup_Figure_3_D_label_df)
+  
+ 
+}
+
+fill_vec_3_D = c("grey70")
+names(fill_vec_3_D) = ribbon_label_3_D
+
+
+
+Sup_Fig_3_D = ggplot(data = Sup_Figure_3_D_df) +
+  geom_ribbon(aes(x = time, ymin = R_0_min, ymax =  R_0_max, fill = ribbon_label_3_D)) +
+  geom_line(aes(x = time, y = value, color = variable)) +
+  geom_point(aes(x = time, y = value, color = variable)) +
+  rahul_theme +
+  theme(legend.text = element_text(size = 12,
+                                   face = "bold",
+                                   color = "black")) +
+  theme_white_background +
+  scale_fill_manual(name = "Ribbon  Legend", values = fill_vec_3_D) +
+  scale_color_manual(name = "Color  Legend", values = c("red"),
+                     labels = c("R_0 (MLE)"))  +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())+
+  xlab("Month") + ylab(expression(R[0])) +
+  scale_x_continuous(breaks = Sup_Figure_3_D_label_df$plot_label_times,
+                     labels = Sup_Figure_3_D_label_df$plot_label_month_names)+
+  facet_wrap(~Model_Name, ncol = 1)
+Sup_Fig_3_D
+pdf("../Figures/Supplemental_Figures/Supplemental_Figure_3/Supplemental_Figure_3D.pdf")
+print(Sup_Fig_3_D)
+dev.off()
+
+Sup_Fig_3_D_comb = Sup_Fig_3_D + rahul_panel_theme + theme(legend.position = "None")
 
 
 pdf("../Figures/Supplemental_Figures/Supplemental_Figure_2/Supplemental_Figure_2_Combined.pdf")
-print(grid.arrange(Sup_Fig_2_A_comb, Sup_Fig_2_B_comb, Sup_Fig_2_C_comb, Sup_Fig_2_D_comb, Sup_Fig_2_E_comb,
-                   legend, ncol = 3))
+print(grid.arrange(Sup_Fig_2_A_comb, Sup_Fig_2_B_comb, Sup_Fig_3_D_comb, 
+                   ncol = 3))
 dev.off()
 
 
